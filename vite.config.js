58bc660ae8f997622c929c3aadf8e66c1a08b97f
rafi-arlab <rafi@thearlab.com>
2026-02-25 13:22:54 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/r3f/',
+  base: process.env.GITHUB_ACTIONS
+    ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || 'r3f'}/`
+    : '/',
   plugins: [react()],
 })
